2682dd02378ae8775a19ca6cb576ed94a00c34d9
MostafaMa9dy <mostafama9dy@gmail.com>
2026-04-06 09:51:07 +0200

    Complete Task CRUD and add clean script

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import * as Models from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  findAll(): Models.Task[] {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Models.Task {
+    return this.tasksService.findOne(id);
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Models.Task {
+    return this.tasksService.create(title, description);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id') id: string,
+    @Body('status') status: Models.TaskStatus,
+  ): Models.Task {
+    return this.tasksService.updateStatus(id, status);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): void {
+    return this.tasksService.remove(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  findAll(status?: TaskStatus): Task[] {
+    if (status) return this.tasks.filter((t) => t.status === status);
+    return this.tasks;
+  }
+
+  findOne(id: string): Task {
+    const task = this.tasks.find((t) => t.id === id);
+    if (!task) throw new NotFoundException(`Task ${id} not found`);
+    return task;
+  }
+
+  create(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  updateStatus(id: string, status: TaskStatus): Task {
+    const task = this.findOne(id);
+    task.status = status;
+    return task;
+  }
+  
+  remove(id: string): void {
+    this.findOne(id);
+    this.tasks = this.tasks.filter((t) => t.id !== id);
+  }
+}
